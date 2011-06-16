fc1d81db980e039bcdbf6fa2581c75c1d2adb735
Bryan Cheng <bryancheng@rescomp-10-149842.Stanford.EDU>
2011-06-15 19:37:25 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swift::Application.config.secret_token = '31882092e09a4431f1ec19a4f6f49162b48172e365232424751c8d5db96cdf87ebe50452d0944b6aa88cfe3b0165a3c8b12c16ce07d3a55db0a1ea25247eb0a5'
